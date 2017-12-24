f163db880b580307bce13ba43b13618f5949ca51
HimanshuP90 <himanshup895@gmail.com>
2017-12-24 22:17:28 +0530

    Added server side validations

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Message } from 'semantic-ui-react';
 import Validator from 'validator';
 import InlineError from '../messages/InlineError';
 import PropTypes from 'prop-types';
@@ -22,7 +22,9 @@ class LoginForm extends React.Component {
 		const errors = this.validate(this.state.data);
 		this.setState({ errors });
 		if (Object.keys(errors).length === 0) {
-			this.props.submit(this.state.data); 
+			this.props
+			.submit(this.state.data)
+			.catch(err => this.setState({ errors: err.response.data.errors}));
 		}
 	}
 
@@ -36,8 +38,12 @@ class LoginForm extends React.Component {
 	render() {
 		const { data, errors } = this.state;
 		return(
-			<div className="ui inverted segment">
-			<Form onSubmit={this.onSubmit}  className="ui inverted form">
+			<Form onSubmit={this.onSubmit}>
+				{ errors.global && <Message negative>
+					<Message.Header>Something went wrong</Message.Header>
+						<p>{errors.global}</p>
+					</Message>
+				}
 				<Form.Field error={!!errors.email}>
 					<label htmlFor="email">Email</label>
 					<input
@@ -62,9 +68,8 @@ class LoginForm extends React.Component {
 						/>
 						{ errors.password && <InlineError text={errors.password} />}
 				</Form.Field>
-				<Button>Login</Button>
+				<Button primary>Login</Button>
 			</Form>
-			</div>
 		);
 	}
 }
