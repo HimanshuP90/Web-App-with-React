c4bdecf873622dd1046a4fe655bc18c38a31b695
HimanshuP90 <himanshup895@gmail.com>
2017-12-25 10:52:18 +0530

    MongoDB integrated for the database

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import path from 'path';
+import mongoose from "mongoose";
+import bodyParser from 'body-parser';
+import auth from './routes/auth';
 
 const app = express();
+app.use(bodyParser.json());
 
-app.post('/api/auth', (req, res) => {
-	res.status(400).json({
-		errors: {
-			global: "Invalid credentials"
-		}
-	});
-});
+mongoose.connect("mongodb://localhost/bookworn", { userMongoClient: true });
+
+app.use('/api/auth', auth);
 
 app.get("/*", (req,res) => {
 	res.sendFile(path.join(__dirname, "index.html"));
